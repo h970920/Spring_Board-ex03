70d66f5272b71743172741276b17703a12e42a3d
h970920 <h970920@naver.com>
2024-03-12 13:08:49 +0900

    레지스터 테스트 완료

--- src/test/java/org/zerock/controller/BoardControllerTest.java
+++ src/test/java/org/zerock/controller/BoardControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -40,4 +42,16 @@ public class BoardControllerTest {
                         .getModelAndView()
                         .getModelMap());
     }
+    
+    @Test
+    public void testRegister() throws Exception{
+       String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+             .param("title", "테스트 새글 제목")
+             .param("content", "테스트 새글 내용")
+             .param("writer", "user00")
+             ).andReturn().getModelAndView().getViewName();
+       
+       log.info(resultPage);
+    }
+    
 }
