a672af7c9ae2bfe6419fd0e89535db3f6d7318b9
h970920 <h970920@naver.com>
2024-03-15 15:17:13 +0900

    타임매퍼 추가

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -19,55 +19,52 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class BoardController {
 
-   private BoardService service;
-   
-   @GetMapping("/list")
-   public void list(Model model) {
-      log.info("list");
-      model.addAttribute("list",service.getList());
-   }
-   
-   @PostMapping("/register") 
-   public String register(BoardVO board, RedirectAttributes rttr) {
-      log.info("register : " + board);
-      service.register(board);
-      rttr.addFlashAttribute("result", board.getBno());
-      return "redirect:/board/list";
-   }
-   
-   @GetMapping("/get")
-   public void get(@RequestParam("bno") long bno,Model model) {
-	   
-	   
-	   log.info("/get");
-	   model.addAttribute("board", service.get(bno));
-   }
-   
-   @PostMapping("/modify")
-   public String modify(BoardVO board, RedirectAttributes rttr) {
-	   log.info("modify : " + board);
-	   
-	   if(service.modify(board)) {
-		   rttr.addAttribute("result : " + "success");
-		   
-	   }
-	   return "redirect:/board/list";
-	   
-   }
-   
-   @PostMapping("/remove")
-   public String remove(@RequestParam("bno") long bno , RedirectAttributes rttr) {
-      log.info("remove : "  + bno );
-      if(service.remove(bno)) {
-          rttr.addFlashAttribute("result", "success");
-      }
-      return "redirect:/board/list";
-   }
-   
-   
-   @GetMapping("/register")
-   public void register() {
-	   
-   }
-   
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list",service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String regiseter(BoardVO board, RedirectAttributes rttr) {
+		log.info("register: " + board);
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/register")
+	public void getRegister() {
+		
+	}
+	
+	@GetMapping({"/get","/modify"})
+	public void get(@RequestParam("bno")Long bno, Model model) {
+		log.info("/get or modify");
+		model.addAttribute("board",service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify:" + board);
+		//성공시 list로 반환시키기
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno")Long bno,RedirectAttributes rttr) {
+		log.info("remove..." + bno);
+		//성공시 list로 반환시키기
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	
 }
--- src/main/java/org/zerock/domain/BoardVO.java
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -1,18 +1,16 @@
 package org.zerock.domain;
 
-import java.util.Date;
+import java.sql.Date;
 
 import lombok.Data;
 
 @Data
 public class BoardVO {
 
-	private long bno;
+	private Long bno;
 	private String title;
 	private String content;
 	private String writer;
 	private Date regdate;
 	private Date updateDate;
-	
-	
 }
