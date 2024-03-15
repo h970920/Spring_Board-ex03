ffa3433abbfb52b8eefece106627d2fb90cb1807
h970920 <h970920@naver.com>
2024-03-15 12:59:14 +0900

    testModify 기능 테스트 완료 / 220p

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -43,4 +43,17 @@ public class BoardController {
 	   log.info("/get");
 	   model.addAttribute("board", service.get(bno));
    }
+   
+   @PostMapping("/modify")
+   public String modify(BoardVO board, RedirectAttributes rttr) {
+	   log.info("modify : " + board);
+	   
+	   if(service.modify(board)) {
+		   rttr.addAttribute("result : " + "success");
+		   
+	   }
+	   return "redirect:/board/list";
+	   
+   }
+   
 }
